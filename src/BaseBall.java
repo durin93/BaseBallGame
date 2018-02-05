40dc0debfd9c0043c1c605e1eb75496dd8133752
USER <USER@USER-6S55DK8135>
2018-02-05 21:04:31 +0900

    0체크 조건 추가. 주석추가

--- src/BaseBall.java
+++ src/BaseBall.java
@@ -5,12 +5,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BaseBall {
+
 	
+	//정답생성
 	public int[] com_Answer() {
 		int[] com_Answer = new int[3];
 		Random rand = new Random();
 		for (int i = 0; i < com_Answer.length; i++) {
-			com_Answer[i] = rand.nextInt(9)+1;
+			com_Answer[i] = rand.nextInt(9) + 1;
 
 			for (int k = 0; k < i; k++) {
 				if (com_Answer[i] == com_Answer[k]) {
@@ -21,29 +23,23 @@ public class BaseBall {
 		return com_Answer;
 	}
 
+	
+	//사용자 정답 받기
 	public int user_Answer(int[] com_Answer) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("숫자를 입력해주세요 ");
 		System.out.print("ex)" + Arrays.toString(com_Answer) + " : ");
-		
+
 		while (!sc.hasNextInt()) {
 			sc.next();
 			System.out.print("숫자만 입력해주세요 ");
 			System.out.print("ex)" + Arrays.toString(com_Answer) + " : ");
 		}
-
 		int user_Answer = sc.nextInt();
-		
-		//X00 일때, XX0일때, X0X,00X일때, 0XX일때  
-		if ((user_Answer % 100) == 0 || (user_Answer % 10) ==0 || (user_Answer % 100) < 10 || (user_Answer / 100) < 1) {
-			System.out.println("0을 제외한 세가지 숫자를 한번에 입력해 주세요.");
-			return 0;
-		}
-		System.out.println("입력한 숫자 : "+user_Answer);
-		return user_Answer;
+		return zeroCheck(user_Answer);
 	}
-	
 
+	//사용자 정답 배열생성
 	public int[] make_user_Answer_arr(int user_Answer) {
 		int[] user_Answer_arr = new int[3];
 		user_Answer_arr[0] = user_Answer / 100;
@@ -52,6 +48,19 @@ public class BaseBall {
 		return user_Answer_arr;
 	}
 
+	//사용자 정답 0체크, 네자리 이상체크
+	public int zeroCheck(int user_Answer) {
+		// X00 일때, XX0일때, X0X,00X일때, 0XX일때 , 네자리이상일때
+		if ((user_Answer % 100) == 0 || (user_Answer % 10) == 0 || (user_Answer % 100) < 10
+				|| (user_Answer / 100) < 1 || (user_Answer / 100) > 9) {
+			System.out.println("0을 제외한 세가지 숫자를 한번에 입력해 주세요.");
+			return 0;
+		}
+		System.out.println("입력한 숫자 : " + user_Answer);
+		return user_Answer;
+	}
+
+	//사용자 정답 중복 체크
 	public int overapCheck(int[] user_Answer_arr) {
 		int count = 0;
 		for (int i = 0; i < user_Answer_arr.length; i++) {
@@ -67,6 +76,7 @@ public class BaseBall {
 		return count;
 	}
 
+	//정답과 사용자 정답 비교
 	public HashMap<String, Integer> test(int[] user_Answer_arr, int[] com_Answer) {
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		int strike = 0;
@@ -84,6 +94,7 @@ public class BaseBall {
 		return map;
 	}
 
+	//출력
 	public int printResult(HashMap<String, Integer> map) {
 		if (map.get("strike") > 0) {
 			System.out.print(map.get("strike") + "스트라이크 ");
@@ -92,7 +103,7 @@ public class BaseBall {
 			System.out.print(map.get("ball") + "볼");
 		}
 		System.out.println();
-		if(map.get("strike")==3){
+		if (map.get("strike") == 3) {
 			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
 			return 1;
 		}
@@ -102,16 +113,18 @@ public class BaseBall {
 	public static void main(String[] args) {
 
 		BaseBall baseBall = new BaseBall();
-		int[] com_Answer = baseBall.com_Answer(); //정답 생성
+		int[] com_Answer = baseBall.com_Answer(); // 정답 생성
 
 		for (;;) {
-			int user_Answer = baseBall.user_Answer(com_Answer); //사용자 답변받기 및 검사
+			int user_Answer = baseBall.user_Answer(com_Answer); // 사용자 답변받기 및 검사
 			if (user_Answer != 0) { // 세자리 검사 통과
 				int[] user_Answer_arr = baseBall.make_user_Answer_arr(user_Answer);
 				if (baseBall.overapCheck(user_Answer_arr) == 0) {// 중복검사 통과
 					HashMap<String, Integer> map = new HashMap<String, Integer>();
-					map = baseBall.test(user_Answer_arr, com_Answer); //스트라이크 볼 검사
-					if(baseBall.printResult(map)!=0)break; //3스트라이크 시 게임종료
+					map = baseBall.test(user_Answer_arr, com_Answer); // 스트라이크 볼
+																		// 검사
+					if (baseBall.printResult(map) != 0)
+						break; // 3스트라이크 시 게임종료
 				}
 			}
 			// for문 닫기
