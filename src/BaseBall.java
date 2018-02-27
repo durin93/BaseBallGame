c81f98553085a9f31f607431269789ad06654b61
USER <USER@USER-6S55DK8135>
2018-02-27 20:00:21 +0900

    update -> depth reduce

--- src/BaseBall.java
+++ src/BaseBall.java
@@ -7,24 +7,41 @@ import java.util.Scanner;
 public class BaseBall {
 
 	
-	//정답생성
+	//공통for문 ddepth 2
+	public boolean commonFor(int arr[], int i){
+		for (int k = 0; k < i; k++) {
+			if (arr[i] == arr[k]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	//정답생성 수정 depth2
 	public int[] com_Answer() {
 		int[] com_Answer = new int[3];
 		Random rand = new Random();
+		
 		for (int i = 0; i < com_Answer.length; i++) {
 			com_Answer[i] = rand.nextInt(9) + 1;
 
-			for (int k = 0; k < i; k++) {
+			if(commonFor(com_Answer, i)){
+				i -= 1;
+			}
+			
+			/*	for (int k = 0; k < i; k++) {
 				if (com_Answer[i] == com_Answer[k]) {
 					i -= 1;
 				}
-			}
+			}*/
+			
 		}
 		return com_Answer;
 	}
 
 	
-	//사용자 정답 받기
+	//사용자 정답 받기 
 	public int user_Answer(int[] com_Answer) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("숫자를 입력해주세요 ");
@@ -39,7 +56,7 @@ public class BaseBall {
 		return zeroCheck(user_Answer);
 	}
 
-	//사용자 정답 배열생성
+	//사용자 정답 배열생성 
 	public int[] make_user_Answer_arr(int user_Answer) {
 		int[] user_Answer_arr = new int[3];
 		user_Answer_arr[0] = user_Answer / 100;
@@ -48,7 +65,7 @@ public class BaseBall {
 		return user_Answer_arr;
 	}
 
-	//사용자 정답 0체크, 네자리 이상체크
+	//사용자 정답 0체크, 네자리 이상체크 depth1
 	public int zeroCheck(int user_Answer) {
 		// X00 일때, XX0일때, X0X,00X일때, 0XX일때 , 네자리이상일때
 		if ((user_Answer % 100) == 0 || (user_Answer % 10) == 0 || (user_Answer % 100) < 10
@@ -60,23 +77,24 @@ public class BaseBall {
 		return user_Answer;
 	}
 
-	//사용자 정답 중복 체크
+	//사용자 정답 중복 체크 수정 depth2
 	public int overapCheck(int[] user_Answer_arr) {
 		int count = 0;
+		
 		for (int i = 0; i < user_Answer_arr.length; i++) {
-			for (int k = 0; k < i; k++) {
-				if (user_Answer_arr[i] == user_Answer_arr[k]) {
-					count++;
-				}
+			
+			if(commonFor(user_Answer_arr, i)){
+				count++;
 			}
 		}
+		
 		if (count != 0) {
 			System.out.println("중복되지 않는 세자리 숫자를 입력해주세요");
 		}
 		return count;
 	}
 
-	//정답과 사용자 정답 비교
+	//정답과 사용자 정답 비교 depth 2
 	public HashMap<String, Integer> test(int[] user_Answer_arr, int[] com_Answer) {
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		int strike = 0;
@@ -94,7 +112,7 @@ public class BaseBall {
 		return map;
 	}
 
-	//출력
+	//출력 depth 1
 	public int printResult(HashMap<String, Integer> map) {
 		if (map.get("strike") > 0) {
 			System.out.print(map.get("strike") + "스트라이크 ");
@@ -110,22 +128,37 @@ public class BaseBall {
 		return 0;
 	}
 
+	
+	
+	//depth2
+	public int gameStart(int[] user_Answer_arr, int[] com_Answer){
+		if (overapCheck(user_Answer_arr) == 0) {// 중복검사 통과
+			HashMap<String, Integer> map = new HashMap<String, Integer>();
+			map = test(user_Answer_arr, com_Answer); // 스트라이크 볼
+																// 검사
+			if (printResult(map) != 0){
+				return 1; // 3스트라이크 시 게임종료
+			}
+		}
+			return 0;
+	}
+	
+	
+	//depth2
 	public static void main(String[] args) {
 
 		BaseBall baseBall = new BaseBall();
 		int[] com_Answer = baseBall.com_Answer(); // 정답 생성
-
+		int flag = 0;
 		for (;;) {
+			if(flag!=0){
+				break;
+			}
+			
 			int user_Answer = baseBall.user_Answer(com_Answer); // 사용자 답변받기 및 검사
 			if (user_Answer != 0) { // 세자리 검사 통과
 				int[] user_Answer_arr = baseBall.make_user_Answer_arr(user_Answer);
-				if (baseBall.overapCheck(user_Answer_arr) == 0) {// 중복검사 통과
-					HashMap<String, Integer> map = new HashMap<String, Integer>();
-					map = baseBall.test(user_Answer_arr, com_Answer); // 스트라이크 볼
-																		// 검사
-					if (baseBall.printResult(map) != 0)
-						break; // 3스트라이크 시 게임종료
-				}
+				flag = baseBall.gameStart(user_Answer_arr, com_Answer);
 			}
 			// for문 닫기
 		}
